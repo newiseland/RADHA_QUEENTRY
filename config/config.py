0eaaaf663ba34a7f0bc7bbea3681baa4dd7feb62
MISHRAJII123 <ccaddnew@gmail.com>
2024-12-10 10:23:28 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -52,7 +52,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/SARKAROP123/SARKARMUSICNEW",
+    "https://github.com/MISHRAJII123/NEWMUSICREPOS",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
@@ -95,7 +95,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SARKAROP123/SARKARMUSICNEW",
+    "https://github.com/MISHRAJII123/NEWMUSICREPOS",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
