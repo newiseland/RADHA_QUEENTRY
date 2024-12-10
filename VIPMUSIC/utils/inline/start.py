35a166fc711612e790c7ee12483593ce0152c00c
MISHRAJII123 <ccaddnew@gmail.com>
2024-12-10 10:39:17 +0530

    Update start.py

--- VIPMUSIC/utils/inline/start.py
+++ VIPMUSIC/utils/inline/start.py
@@ -30,34 +30,26 @@ def private_panel(_):
             InlineKeyboardButton(
                 text="❍ 𝐀ᴅᴅ 𝐌ᴇ 𝐘ᴏᴜʀ 𝐆ʀᴏᴜᴘ ❍",
                 url=f"https://t.me/{app.username}?startgroup=true",
-            ),
+            )
         ],
         [
             InlineKeyboardButton(
-                text="❍ 𝐘ᴏᴜʀ 𝐇ᴇʟᴘᴇʀ ❍",
-                callback_data="settings_back_helper",
-            ),
+                text="❍ 𝐘ᴏᴜʀ 𝐇ᴇʟᴘᴇʀ ❍", callback_data="settings_back_helper"),
         ],
         [
-            InlineKeyboardButton(
+             InlineKeyboardButton(
                 text="❍ 𝐎ᴡɴᴇʀ ❍",
-                url=f"https://t.me/ll_SARKAR_MERA_BABU_ll",
+                url=f"https://t.me/ll_SARKAR_OWNER_ll",
             ),
             InlineKeyboardButton(
                 text="❍ 𝐀ʟʟ 𝐁ᴏᴛs ❍",
-                url=f"https://t.me/TG_NAME_STYLE/4368",
-            ),
+                url=f"https://t.me/PROMOTION_UPDATE/6",
+            )
         ],
         [
             InlineKeyboardButton(
                 text="❍ 𝐏𝐑𝐎𝐌𝐎𝐓𝐈𝐎𝐍 𝐀𝐕𝐀𝐈𝐋𝐀𝐁𝐋𝐄 ❍",
                 url=f"https://t.me/TG_NAME_STYLE/4602",
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="❍ 𝐌𝐀𝐍𝐀𝐆𝐄𝐌𝐄𝐍𝐓 ❍",
-                url=f"help_back",
             ),
         ],
     ]
