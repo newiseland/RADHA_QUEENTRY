e426d176793ff7c1574a950b7b34e761d4534f77
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-12-20 13:03:10 +0530

    Update __init__.py

--- VIPMUSIC/utils/__init__.py
+++ VIPMUSIC/utils/__init__.py
@@ -1,16 +1,7 @@
-#
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
-#
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 from .channelplay import *
 from .database import *
 from .decorators import *
+from .extraction import *
 from .formatters import *
 from .inline import *
 from .pastebin import *
