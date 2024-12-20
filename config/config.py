c1cf99468f8364df86f962b766b369e2db522c36
𓆩◕🇭𝐀𝐑𝐑𝐘◕𓆪 =‌𐏓 𝄗⃝🇮🇳™ <herukiraj@gmail.com>
2024-12-20 12:33:22 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -71,7 +71,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MISHRAJII123/NEWMUSICREPOS",
+    "https://github.com/newiseland/RADHA_QUEENTRY",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
